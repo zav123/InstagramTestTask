ac2e17c7712a8bb1f0a1fdb56523f5c49370c075
Zakharin Alexander <zav333@gmail.com>
2013-06-08 11:18:32 +0400

    - first commit

---
+++ App/Insta/Insta/ZAVAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ZAVAppDelegate.h
+//  Insta
+//
+//  Created by admin on 08.06.13.
+//  Copyright (c) 2013 zav333. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZAVAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ App/Insta/Insta/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Insta
+//
+//  Created by admin on 08.06.13.
+//  Copyright (c) 2013 zav333. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ZAVAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ZAVAppDelegate class]));
+    }
+}
