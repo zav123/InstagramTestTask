fe18a516de60fa1e1f4c6c3767171df71272d400
Zakharin Alexander <zav333@gmail.com>
2013-06-08 13:36:37 +0400

    - smallFix Login

--- App/Insta/LoginVC.m
+++ App/Insta/LoginVC.m
@@ -10,9 +10,16 @@
 
 #import "LoginVC.h"
 
-#define AuthorizationLink @"https://instagram.com/oauth/authorize/?client_id=%@&redirect_uri=%@&response_type=code"
-#define Cliant_ID @"324b9a0a3d044e109270bbad54f68ac3"
-#define Redirect_uri @"http://easydevios.blogspot.ru/123"
+#define AuthorizationLink @"https://instagram.com/oauth/authorize/?client_id=%@&redirect_uri=%@&response_type=token"
+#define Cliant_ID @"8cdc54227130438f9232c710e74d9258"
+#define Redirect_uri @"http://localhost:8888/MAMP/"
+
+
+
+
+
+
+
 
 @interface LoginVC () {
     
@@ -23,34 +30,46 @@
 
 @implementation LoginVC
 
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    _webView = [[UIWebView alloc] initWithFrame:self.view.frame];
-    
-    NSURL *url = [NSURL URLWithString:[NSString stringWithFormat:AuthorizationLink, Cliant_ID, Redirect_uri]];
-    [_webView loadRequest:[NSURLRequest requestWithURL:url]];
+
+    _webView = [[UIWebView alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height)];
+    NSString *fullURL = [NSString stringWithFormat:AuthorizationLink, Cliant_ID, Redirect_uri];
+    NSURL *url = [NSURL URLWithString:fullURL];
+    NSURLRequest *requestObj = [NSURLRequest requestWithURL:url];
+    [_webView loadRequest:requestObj];
     _webView.delegate = self;
-    
     [self.view addSubview:_webView];
 }
 
--(void)webViewDidFinishLoad:(UIWebView *)webView {
-    if ([_webView.request.URL.absoluteString rangeOfString:@"code="].location != NSNotFound) {
-        NSArray *fierstPart = [[[[webView request] URL] absoluteString] componentsSeparatedByString:@"code="];
-        NSString *fierstPatsStr = [fierstPart lastObject];
-        NSArray *secondArr = [fierstPatsStr componentsSeparatedByString:@"&"];
-        
-        if (secondArr.count > 0) {
-            NSString *token = secondArr[0];
+-(BOOL)webView:(UIWebView *)webView shouldStartLoadWithRequest:(NSURLRequest *)request navigationType:(UIWebViewNavigationType)navigationType{
+    
+    NSString* urlString = [[request URL] absoluteString];
+    NSURL *Url = [request URL];
+    NSArray *UrlParts = [Url pathComponents];
+    if ([[UrlParts objectAtIndex:(1)] isEqualToString:@"MAMP"]) {
+        NSRange tokenParam = [urlString rangeOfString: @"access_token="];
+        if (tokenParam.location != NSNotFound) {
+            NSString* token = [urlString substringFromIndex: NSMaxRange(tokenParam)];
+
+            NSRange endRange = [token rangeOfString: @"&"];
+            if (endRange.location != NSNotFound)
+                token = [token substringToIndex: endRange.location];
             
-            if(token){
+            NSLog(@"access token %@", token);
+            if ([token length] > 0 ) {
                 [[NSUserDefaults standardUserDefaults] setObject:token forKey:@"AccessToken"];
                 [[NSUserDefaults standardUserDefaults] synchronize];
             }
         }
+        return NO;
     }
+    return YES;
 }
+
+
 
 
 @end
